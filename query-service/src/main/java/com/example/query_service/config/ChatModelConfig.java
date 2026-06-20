29f224d3c993b0a5813da0cea5878d69774e210d
Ashish Jha <jhaashish270@gmail.com>
2026-06-20 18:16:15 +0530

    DockerFile and updated dockercompose.

---
+++ query-service/src/main/java/com/example/query_service/config/ChatModelConfig.java
@@ -0,0 +1,53 @@
+package com.example.query_service.config;
+
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ChatModelConfig {
+
+    private final RedisChatMemory redisChatMemory;
+
+    @Bean
+    public ChatLanguageModel chatLanguageModel(
+            @Value("${groq.baseurl}")
+            String url,
+            @Value("${groq.api.key}")
+            String api,
+            @Value("${groq.model}")
+            String model
+    ) {
+
+        return OpenAiChatModel.builder()
+                .baseUrl(url)
+                .apiKey(api)
+                .modelName(model)
+                .maxTokens(600)
+                .temperature(0.2)
+                .build();
+    }
+
+    @Bean
+    public ChatAssistant chatAssistant(
+            ChatLanguageModel chatLanguageModel
+    ){
+        return AiServices.builder(ChatAssistant.class)
+                .chatLanguageModel(chatLanguageModel)
+                .chatMemoryProvider(memoryID ->
+                        MessageWindowChatMemory
+                                .builder()
+                                .id(memoryID)
+                                .chatMemoryStore(redisChatMemory)
+                                .maxMessages(5)
+                                .build()
+                )
+                .build();
+    }
+}
---
+++ query-service/src/main/java/com/example/query_service/service/CodeChunkRetriever.java
@@ -0,0 +1,81 @@
+package com.example.query_service.service;
+
+import com.example.query_service.dtos.ScoredChunk;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.grpc.JsonWithInt;
+import io.qdrant.client.grpc.Points;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@RequiredArgsConstructor
+@Component
+public class CodeChunkRetriever {
+
+    private final EmbeddingModel embeddingModel;
+    private final QdrantClient qdrantClient;
+
+    private static final int TOP_K = 5;
+    public List<ScoredChunk> retriever(String question,String namespace) throws ExecutionException, InterruptedException {
+
+        Embedding embedding = embeddingModel.embed(question).content();
+        float[] vec = embedding.vector();
+
+        Points.Filter filter = Points.Filter.newBuilder()
+                .addMust(Points.Condition.newBuilder()
+                                .setField(Points.FieldCondition.newBuilder()
+                                        .setKey("namespace")
+                                        .setMatch(Points.Match.newBuilder()
+                                                .setKeyword(namespace)
+                                                .build()
+                                        ).build()
+
+                                ).build()
+                ).build();
+
+        List<Points.ScoredPoint> points = qdrantClient.searchAsync(
+                        Points.SearchPoints.newBuilder()
+                                .setCollectionName("code_chunks")
+                                .setFilter(filter)
+                                .setLimit(TOP_K)
+                                .addAllVector(toFloatList(vec))
+                                .setWithPayload(Points.WithPayloadSelector.newBuilder().setEnable(true).build())
+                                .build()
+                ).get();
+
+        return points.stream()
+                .map(this::toChodChunk)
+                .toList();
+    }
+
+    private ScoredChunk toChodChunk(Points.ScoredPoint point) {
+
+        Map<String, JsonWithInt.Value> p = point.getPayloadMap();
+
+        return new ScoredChunk(
+                p.get("filePath").getStringValue(),
+                (int) p.get("startLine").getIntegerValue(),
+                (int) p.get("endLine").getIntegerValue(),
+                p.get("chunkType").getStringValue(),
+                p.get("content").getStringValue(),
+                point.getScore()
+        );
+    }
+
+    private Iterable<Float> toFloatList(float[] vec) {
+        List<Float> lst = new ArrayList<>();
+
+        for (float v: vec) {
+            lst.add(v);
+        }
+
+        return lst;
+    }
+
+}
